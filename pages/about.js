b70f284dc41c7f6eddc4ed3c31e590d8bbb91714
Shuwei Liu <lsw2914@hotmail.comm>
2021-11-17 17:18:18 +1100

    Add contact information

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import TwoColumnLists from "../components/games/TwoColumnLists";
+import { AiOutlineMail, AiFillGithub } from "react-icons/ai";
 
 export default function About() {
   return (
@@ -6,8 +8,8 @@ export default function About() {
       <Head>
         <title>Free Games | About</title>
       </Head>
-      <div className="container text-white p-5 sort-font fs-3 p-indent">
-        <p>
+      <div className="container text-white p-5 sort-font fs-3">
+        <p className="p-indent">
           This web app is built based on the API provided by the{" "}
           <a href={process.env.NEXT_PUBLIC_ORIGINAL_SITE} target="_blank">
             Free-To-Game
@@ -24,13 +26,41 @@ export default function About() {
           .
         </p>
 
-        <p className="mt-5">
+        <p className="mt-5 p-indent">
           You can use this site to find some interesting games to pass your free
           time. The current functionalities of this web app are not very
           complete because of the limitation of the API, but I will try to
           enrich more functionalities in the future. Feel free to explore any
-          free games online and enjoy your time.
+          free games online and enjoy your time!
         </p>
+        <br />
+        <footer className="border-top border-secondary p-5">
+          <div className="row">
+            <TwoColumnLists
+              list={{
+                "author:": "Shuwei(Shawn) Liu",
+                "latest update:": "17/11/2021",
+                "e-mail:": (
+                  <a
+                    href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}
+                    style={{ color: "inherit" }}
+                  >
+                    <AiOutlineMail size={40} /> {process.env.NEXT_PUBLIC_EMAIL}
+                  </a>
+                ),
+                "git-hub:": (
+                  <a
+                    href={process.env.NEXT_PUBLIC_GIT_HUB}
+                    style={{ color: "inherit" }}
+                    target="_blank"
+                  >
+                    <AiFillGithub size={40} />
+                  </a>
+                ),
+              }}
+            />
+          </div>
+        </footer>
       </div>
     </>
   );
